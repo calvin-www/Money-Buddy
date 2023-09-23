3e1b4e1949edea34a9d8f26599b55a211e38b341
Casey Fan <fan.casey23@gmail.com>
2023-09-23 15:41:17 -0500

    resolved conflicts

--- bot.py
+++ bot.py
@@ -12,20 +12,13 @@ async def on_ready():
 
 async def main():
 	try:
-<<<<<<< HEAD
 		await bot.load_extension("cogs.addExpense")
 		await bot.load_extension("cogs.report")
-=======
 		await bot.load_extension("cogs.categories")
->>>>>>> category-select
 		print(f'Extension loaded!')
 	except Exception as e:
 		print(f'Failed to load extension cogs.')
 		print(str(e))
-<<<<<<< HEAD
 	await bot.start('MTE1NDk4MTk2MzY0NDU0NzEzMg.GBTMb8.OZGGh8IBy-lkwNgDN3XHHt2AlIxZhaYUsFwfdU')
-=======
-	await bot.start('MTE1NDk3MTUzMTY2MzkxNzE3Ng.GDBG-k.Nmk3LAH3VjoIB-_7eKWA42uaxXJ0sHo6AWiE50')
->>>>>>> category-select
 
 asyncio.run(main())
