c44f3fa74e05e6cf4098e126b19fb038466005db
Moyinoluwa Orimoloye <moyindavid@moyinoluwas-mbp.dyndns.rice.edu>
2023-09-23 15:15:10 -0500

    completed add expense and add category functionality

--- cogs/addExpense.py
+++ cogs/addExpense.py
@@ -4,6 +4,10 @@ import asyncio
 from .vars import *
 from discord import ui
 
+async def addCategory(ctx: commands.Context, args: list):
+    expenses[args[0]]=[]
+    return await ctx.channel.send(f"You have successfully added the {args[0]} category")
+
 # cogs let you put related commands and functions together under a class
 class AddExpense(commands.Cog):
 
@@ -13,17 +17,28 @@ class AddExpense(commands.Cog):
     @commands.command(name='add')
     async def add(self, ctx: commands.Context, *args: str):
         args = list(args)
-        print(args)
         
         if len(args) == 1:
             if args[0] in expenses:
                 return await ctx.channel.send(f"{args[0]} is already a category")
-            expenses[args[0]]={}
-            return await ctx.channel.send(f"You have successfully added the {args[0]} category")
-        else:
-            return await ctx.channel.send("You have to add in one of these formats\n1. add category\n2. add category date amount decsription(optional)")
-        await ctx.channel.send(f'The count is {count[0]}')
+            return await addCategory(ctx, args)
         
+        if len(args) >= 3:
+            desc = ""
+            if len(args)>=4:
+                desc = " ".join(args[3:])
+            if args[0] not in expenses:
+                await addCategory(ctx, args)
+            if [args[1], args[2], desc] in expenses[args[0]]:
+                return await ctx.send("Sorry, you added that expense previously")
+            expenses[args[0]].append([args[1], args[2], desc])
+            return await ctx.channel.send("Success")
+        return await ctx.channel.send("You have to add in one of these formats\n1. add category\n2. add category date amount decsription(optional)")
+       
+
+    @commands.command(name="show")
+    async def show(self, ctx: commands.Context):
+        return await ctx.send(expenses)
 
 
 # add this cog to the client
