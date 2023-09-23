a4f6fda0717c63df2ea668e6bc5be29086a9cce6
Moyinoluwa Orimoloye <moyindavid@moyinoluwas-mbp.dyndns.rice.edu>
2023-09-23 00:17:32 -0500

    established command call

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import asyncio
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix = commands.when_mentioned_or('!'), intents = intents)
+bot = commands.Bot(command_prefix = commands.when_mentioned_or(''), intents = intents)
 
 @bot.event
 async def on_ready():
@@ -12,13 +12,12 @@ async def on_ready():
 
 async def main():
 	try:
-		await bot.load_extension("cogs.rps")
 		await bot.load_extension("cogs.util")
 		print(f'Extension loaded!')
 	except Exception as e:
 		print(f'Failed to load extension cogs.')
 		print(str(e))
-	await bot.start('INSERT YOUR OWN TOKEN HERE!!!')
+	await bot.start('MTE1NDk3MDUxNzMwMDg1NDgzNg.GHYVd8.MrkbMtYI3GSsmtss1KS-0dFgjSPZJcy2Oil-6c')
 
 asyncio.run(main())
 
