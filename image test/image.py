8ba55e1d8c1e5d04cef2e2702ae3d0ad275f0de1
calvin <calvintwong25@gmail.com>
2023-09-23 15:43:41 -0500

    remove image test

--- image test/image.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import pytesseract
-
-im = Image.open(r'C:\Users\calvi\OneDrive\Desktop\github\HackRice13-Bot\imagetext02.jpg')
-
-text = pytesseract.image_to_string(im)
-
-print(text)
