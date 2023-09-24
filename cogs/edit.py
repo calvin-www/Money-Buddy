32684c69749f3486c18c7eb91ff0dbf4f6fbb839
calvin <calvintwong25@gmail.com>
2023-09-23 23:58:22 -0500

    edge cases for edit

--- cogs/edit.py
+++ cogs/edit.py
@@ -14,16 +14,23 @@ class edit(commands.Cog):
             await ctx.send("```edit [category]``` or ```edit [category] [expense number]```")
         elif len(args) == 1:
             cat = args[0]
-            new_catagory =  await prompt1(self.client,ctx,'what would you like to change ' + cat + ' to?',timeout=20)
-            expenses[ctx.author.id][new_catagory] = expenses[ctx.author.id].pop(cat)
-            await ctx.send('Successfully changed ' + cat + ' to '+ new_catagory+'!')
-            update_db()
+            if cat not in expenses[ctx.author.id]:
+                await ctx.send("Sorry that doesn't seem to exist :(")
+            else:
+                new_catagory =  await prompt1(self.client,ctx,'what would you like to change ' + cat + ' to?',timeout=20)
+                expenses[ctx.author.id][new_catagory] = expenses[ctx.author.id].pop(cat)
+                await ctx.send('Successfully changed ' + cat + ' to '+ new_catagory+'!')
+                update_db()
         elif len(args) == 2:
             args = list(args)
-            new_expense = await prompt2(self.client,ctx,'what would you like to change ' + str(expenses[ctx.author.id][str(args[0])][int(args[1]) - 1]) + ' to?', timeout=20 )
-            expenses[ctx.author.id][args[0]][int(args[1]) - 1] = new_expense
-            update_db()
-            await ctx.send("Successfully changed to " + str(new_expense)+ '!')
+            cat = args[0]
+            if cat not in expenses[ctx.author.id] or args[1] not in expenses[ctx.author.id][cat]:
+                await ctx.send("Sorry that doesn't seem to exist :(")
+            else:
+                new_expense = await prompt2(self.client,ctx,'what would you like to change ' + str(expenses[ctx.author.id][str(args[0])][int(args[1]) - 1]) + ' to?', timeout=20 )
+                expenses[ctx.author.id][args[0]][int(args[1]) - 1] = new_expense
+                update_db()
+                await ctx.send("Successfully changed to " + str(new_expense)+ '!')
         else:
             await ctx.send('please format yor edit request as edit [catagory] or edit [catagory] [# of expense]')
 
